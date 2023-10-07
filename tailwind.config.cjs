25b672e3ba2600bf624f16932dc04c15b0d38219
Marios Mitsios <xrteach@gmail.com>
2023-10-08 01:31:20 +0300

    feat: add AI search page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,13 +3,14 @@ const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
   theme: {
-    extend: {},
-    colors: {
-      ...colors,
-      navgreen: "#41E296",
-      background: "#177F3F",
+    extend: {
+      colors: {
+        navgreen: "#41E296",
+        background: "#177F3F",
+        darkbackground: "#86AC86",
+      },
     },
   },
 };
