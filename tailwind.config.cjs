0fc7b14e672d541e216f9f9d6b4de0e133904d06
Nikos Tsilas <tsilasnikos@gmail.com>
2023-10-08 13:01:28 +0300

    finalize home page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,18 @@
-const colors = require("tailwindcss/colors");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
   theme: {
-    extend: {},
-    colors: {
-      ...colors,
-      navgreen: "#41E296",
-      background: "#177F3F",
+    extend: {
+      colors: {
+        navgreen: "#41E296",
+        background: "#177F3F",
+        darkbackground: "#86AC86",
+        greenbtn: "#52FF00",
+        mustard_yellow: "#EAC435",
+        like_blue: "#0EFBCC",
+        smoky_blue: "#5982C5",
+      },
     },
   },
 };
